8dc98ef162c1b1ed74c6867f058abcd5d5548d06
rui-s-costa <89304646+rui-s-costa@users.noreply.github.com>
2022-01-07 11:59:21 -0500

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -21,25 +21,25 @@ provider "google" {
   project = "${var.project}"
 }
 
-module "vpc" {
-  source  = "../../modules/vpc"
-  project = "${var.project}"
-  env     = "${local.env}"
-}
+# module "vpc" {
+#   source  = "../../modules/vpc"
+#   project = "${var.project}"
+#   env     = "${local.env}"
+# }
 
-module "http_server" {
-  source  = "../../modules/http_server"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
+# module "http_server" {
+#   source  = "../../modules/http_server"
+#   project = "${var.project}"
+#   subnet  = "${module.vpc.subnet}"
+# }
 
-module "firewall" {
-  source  = "../../modules/firewall"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
+# module "firewall" {
+#   source  = "../../modules/firewall"
+#   project = "${var.project}"
+#   subnet  = "${module.vpc.subnet}"
+# }
 
-resource "google_dataproc_cluster" "simplecluster" {
-  name   = "simplecluster"
-  region = "us-central1"
-}
+# resource "google_dataproc_cluster" "simplecluster" {
+#   name   = "simplecluster"
+#   region = "us-central1"
+# }
